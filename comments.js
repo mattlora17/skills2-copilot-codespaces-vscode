90a16a3bf8d254d7025d0274b1e60d85c034bb03
mattlora17 <mattlora17@gmail.com>
2024-03-02 19:18:04 +0000

    3

--- comments.js
+++ comments.js
@@ -1,21 +1,23 @@
-// create a web server that listens on port 3000 and serves a file called comments.html
-// when a request is made to the server, it should log the request method and request url
-// to the console
-// Use the createServer method from the http module to create a server
-// Use the fs module to read the comments.html file and serve it as the response
-// Use the listen method to listen on port 3000
+// create a web server that listens on port 3000
+// when a request comes in, read the comments.json file
+// respond with the contents of the file
+// if the file is not found, respond with a 404 status code
 
-// Load the http module to create an http server.
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  console.log(request.method);
-  console.log(request.url);
-  response.writeHead(200, {"Content-Type": "text/html"});
-  fs.createReadStream('./comments.html').pipe(response);
+const server = http.createServer((req, res) => {
+  fs.readFile('comments.json', (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Resource not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(data);
+    }
+  });
 });
 
-// Listen on port 3000, IP defaults to
-
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
